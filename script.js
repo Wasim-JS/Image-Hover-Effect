aa26717e76f43d0c01f5cc76c8236a57b6dd39f2
Wasim Shariff <wasimshariff.02@gmail.com>
2024-06-26 23:13:51 +0530

    inital commit

---
+++ script.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 1500; i++) {
+    const span = document.createElement('span')
+    console.log('con is ',document.querySelector('.container'))
+    document.querySelector('.container').append(span)
+    
+}
